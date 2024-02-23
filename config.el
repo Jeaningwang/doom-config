c70ccfca5de8a9031584793d56821d6045f696e2
dong.wang <moneatts@outlook.com>
2024-02-23 17:03:45 +0800

    feat(mode line 修改)

--- config.el
+++ config.el
@@ -221,3 +221,34 @@
   (add-hook! 'writeroom-mode-hook
             :append #'set-writeroom-width)
   )
+
+
+;;--------------------------------------------------------------------------
+;;-------------------------mode line----------------------------------------
+;;--------------------------------------------------------------------------
+(use-package! doom-modeline
+  :custom-face
+  (mode-line ((t (:height 1.0))))
+  (mode-line-inactive ((t (:height 0.95))))
+  :custom
+  (doom-modeline-height 16)
+  (doom-modeline-bar-width 4)
+  (doom-modeline-lsp nil)
+  (doom-modeline-modal-icon t)
+  (doom-modeline-minor-modes nil)
+  (doom-modeline-major-mode-icon t)
+
+  (defun doom-modeline-conditional-buffer-encoding ()
+    "We expect the encoding to be LF UTF-8, so only show the modeline when this is not the case"
+    (setq-local doom-modeline-buffer-encoding
+                (unless (and (memq (plist-get (coding-system-plist buffer-file-coding-system) :category)
+                                   '(coding-category-undecided coding-category-utf-8))
+                             (not (memq (coding-system-eol-type buffer-file-coding-system) '(1 2))))
+                  t)))
+
+  (add-hook 'after-change-major-mode-hook #'doom-modeline-conditional-buffer-encoding) (doom-modeline-buffer-state-icon t))
+
+(setq display-time-24hr-format t                ;; Display 24 Hrs rather than 12
+      display-time-default-load-average nil     ;; Do not display my CPU Load
+      )
+(display-time-mode 1)
