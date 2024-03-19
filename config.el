9b5cbb5c3b059a1d262e86249cec687b4d3bf05f
dong.wang <moneatts@outlook.com>
2024-03-19 15:01:24 +0800

    feat(添加一个 theme)

--- config.el
+++ config.el
@@ -103,7 +103,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; (setq doom-theme 'doom-one)
-(let ((custom-theme-list (append '(doom-one dtsdh-light oom-henna doom-dracula doom-snazzy doom-oksolar-dark) (custom-available-themes)))
+(let ((custom-theme-list (append '(doom-one doom-flatwhite dtsdh-light oom-henna doom-dracula doom-snazzy doom-oksolar-dark) (custom-available-themes)))
       )
   (setq custom-theme-list-remove '(doom-bluloco-light doom-plain-dark doom-gruvbox-light doom-oksolar-light doom-acario-light leuven doom-solarized-light doom-homage-white dichromacy adwaita modus-operandi))
   (setq custom-theme-list-final (cl-remove-if (lambda (x) (member x custom-theme-list-remove)) custom-theme-list))
